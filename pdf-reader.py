aa9926f0aaba51d7e3e11ec4c921bc50cc1d58b4
Magdum <magdunium@gmail.com>
2015-07-26 16:26:03 +0200

    Added: After scraping files script moves PDFs to directory "Done". If it doesn't exist - creates one.
    Solved problem with not closing the PDF files

--- pdf-reader.py
+++ pdf-reader.py
@@ -5,16 +5,21 @@ __version__ = '1.0 beta'
 import pyPdf
 import re
 import glob
+import shutil
+import os
 
 def getPDFContent(path):
     content = ''
     # Load PDF into pyPDF:
-    pdf = pyPdf.PdfFileReader(file(path, 'rb'))
+    f = file(path, 'rb')
+    pdf = pyPdf.PdfFileReader(f)
     # Iterate pages:
     for i in range(0, pdf.getNumPages()):
         # Extract text from page and add to content:
         content += pdf.getPage(i).extractText() + '\n'
+    f.close()
     return content
+
 
 def matchURLs(value):
     ret = []
@@ -24,6 +29,8 @@ def matchURLs(value):
 
 
 def checkAllFiles():
+    f = open('URLlist.txt', 'w+')
+    allLinkArray = []
     for files in glob.glob('*.pdf'):
         pdfContent = getPDFContent(files)
         urls = matchURLs(pdfContent)
@@ -35,11 +42,21 @@ def checkAllFiles():
             elif url == 'http://www.yourdomain.com' or url == 'http://yourdomain.com':
                 continue
             else:
-                f.writelines(url + '\n')
+                allLinkArray.append(url)
+                f.write(url + '\n')
     f.close()
 
+def moveFilesAfterScraping():
+    source = os.listdir('.')
+    destination = '.\Done'
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for files in source:
+        if files.endswith('.pdf'):
+            shutil.move(files, destination)
 
 if __name__ == '__main__':
-    f = open('URLlist.txt', 'w+')
     checkAllFiles()
+    moveFilesAfterScraping()
     print "Koniec programu"
+
